eb2cf952bddcd589d643741aefeee5e726d36618
Jindřich Bär <jindrichbar@gmail.com>
2023-07-18 08:50:35 +0200

    fix: simplify bundle banners for patch-package

--- scripts/getBundleBanner.js
+++ scripts/getBundleBanner.js
@@ -1,12 +1,5 @@
-import { execSync } from 'child_process';
-
 export function getBundleBanner(pkg) {
-  const lastCommitHash = execSync('git rev-parse --short HEAD')
-    .toString()
-    .trim();
-  const version = process.env.SHIPJS
-    ? pkg.version
-    : `${pkg.version} (UNRELEASED ${lastCommitHash})`;
+  const version = pkg.version;
   const authors = '© Algolia, Inc. and contributors';
 
   return `/*! ${pkg.name} ${version} | MIT License | ${authors} | ${pkg.homepage} */`;
